dfc95aa21bbc2214bf8a4e605add028f0f0199b2
George Moore <38229603+mmoore96@users.noreply.github.com>
2020-03-03 17:57:08 -0500

    Update serverSide.py

--- src/Receiver/serverSide.py
+++ src/Receiver/serverSide.py
@@ -5,7 +5,7 @@ import sys
 from socket import AF_INET, SOCK_DGRAM
 #initializing host, port
 serverAddress = "localhost"
-serverPort = 10032
+serverPort = 10031
 #starting the server
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.settimeout(15);
@@ -13,37 +13,40 @@ server_address = (serverAddress, serverPort)
 s.bind(server_address)
 totalTime = 0
 print('I am ready for any client side request \n')
-totalFilesCount = 100
+totalFilesCount = 10
 i=0;
 fileName = 'receive.txt';
-adk = 1
+adk = 'ok'.encode('utf8')
 while True:
     #s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     #recording the start time
     #s.bind(server_address)
     startTime = datetime.now()
     i = i+1
-    print(i)
+    #print(i)
     file = 'receive'+str(i)+'.txt';
-    
-    print('I am starting receiving file ', fileName, 'for the ',i,'th time')
+
     #opening the file to write
     #data, server = s.recvfrom(1024)
     f = open(file, 'wb')
     data, server = s.recvfrom(1024)
+    print('I am starting receiving file ', fileName, 'for the ',i,'th time')
     #data = data
     #print(data)
+    check = True
     while data:
         f.write(data)
-        s.settimeout(15)
-        #try:
-            #sent = s.sendto(adk, server_address)
-        #except:
-            #print("did not send adk") 
-            #print(len(adk))   
+        #s.settimeout(15)
+        try:
+            s.settimeout(15)
+            sent = s.sendto(adk, server)
+        except:
+            print("did not send adk")
+            #check = False  
         data, server = s.recvfrom(1024)
-        print(data)
+        #print(data)
         if data.decode('utf8') == '-1':
+            sent = s.sendto(adk, server)
             f.close
             break
         
@@ -55,9 +58,10 @@ while True:
     timeTaken = int((endTime - startTime).total_seconds() * 1000)
     totalTime += timeTaken
     print('The time used in millisecond to receive ', fileName ,' for ', i,'th time is: ',timeTaken,'\n')
-    if i == totalFilesCount: break;
+    if i == totalFilesCount:   
+        break;
     try:
-        sent = s.sendto(adk, server_address)
+        sent = s.sendto(adk, server)
     except:
         print("did not send adk") 
     #s.close()
@@ -72,8 +76,3 @@ for x in range(totalFilesCount):
     if res == False: f += 1
 print(f , ' Times out of ' , totalFilesCount , ' are not correct!')
 print('I am done')
-
-    
-
-
-
