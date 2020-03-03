0c2c27a48a4aac207b4d1c1261b3949a8089274f
George Moore <38229603+mmoore96@users.noreply.github.com>
2020-03-03 17:56:44 -0500

    Update clientSide.py

--- src/Sender/clientSide.py
+++ src/Sender/clientSide.py
@@ -6,14 +6,14 @@ import filecmp
 import os
 #initializing host, port, filename, total time and number of times to send the file
 serverAddress = "localhost"
-serverPort = 10032
+serverPort = 10031
 #sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 #server_address = (serverAddress, serverPort)
 fileName = "send.txt"
 totalTime = 0
-numTimesSend = 100
+numTimesSend = 10
 print('I am connecting to server side: ', serverAddress,'\n')
-adk = "-1".encode('utf8')
+eof = "-1".encode('utf8')
 #using a for loop to send the file 100 times 
 
 for x in range(numTimesSend):
@@ -44,43 +44,53 @@ for x in range(numTimesSend):
     for j in range (int(packetsToSend + 1)):
         sent = sock.sendto(data, server_address)
         j+=1
-        print("sending")
+
+        #print("sending")
+
         #reading the next 1024 bits
-        sock.settimeout(5)
+        #sock.settimeout(5)
         #adk, server = sock.recvfrom(1)
         #print(adk)
-        #try:
-            #sock.settimeout(5)
-            #adk, server = sock.recvfrom(2)
+        try:
+            sock.settimeout(5)
+            adk, server = sock.recvfrom(1024)
             #print(adk)
-            #if adk == 1:
-        data = file_to_send.read(1024)
+        except:
+            print("did not get adk")
+            break    
+        if adk.decode('utf8') == 'ok':
+            data = file_to_send.read(1024)
+        else:
+            print("did not get adk")
         if data == b'':
             test = False
-            print("empty test")
+            #print("empty test")
             break
                 #print("got adk")
             #else:
                 #print("did not get adk from server")
         #except:
-            #print("time out reached")
-            #test = False
-            #break            
+        #print("time out reached")
+        #test = False
+        #break            
             
     #try:
     sock.settimeout(5)
-    sent = sock.sendto(adk, server_address)
-    print(adk)
+    sent = sock.sendto(eof, server)
+    #print(eof)
+    data, server = sock.recvfrom(1024)
+    #print(data)
+    if data.decode('utf8') == 'ok':
     
-    print('I am finishing sending file', fileName,' for the ',x,'th  time')
-    file_to_send.close()
-    #recording the end time
-    endTime = datetime.now()
-    timeTaken = int((endTime - startTime).total_seconds() * 1000)
-    totalTime += timeTaken
-    print('The time used in millisecond to receive ', fileName ,' for ', x,'th time is: ',timeTaken,"\n")
-    #except:
-    sock.close()
+        print('I am finishing sending file', fileName,' for the ',x,'th  time')
+        file_to_send.close()
+        #recording the end time
+        endTime = datetime.now()
+        timeTaken = int((endTime - startTime).total_seconds() * 1000)
+        totalTime += timeTaken
+        print('The time used in millisecond to receive ', fileName ,' for ', x,'th time is: ',timeTaken,"\n")
+        #except:
+        sock.close()
 #sock.close()
 print('The average time to receive file ',fileName,' in millisecond is: ',totalTime/numTimesSend)
 print('Total time to receive file ',fileName,' for ',numTimesSend,' times in millisecond is: ',totalTime)
