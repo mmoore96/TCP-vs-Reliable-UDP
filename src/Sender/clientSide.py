8bd36901ce9ea77c7493b2e3f0d8868b0a9f17f3
George Moore <38229603+mmoore96@users.noreply.github.com>
2020-03-03 13:21:03 -0500

    Update clientSide.py

--- src/Sender/clientSide.py
+++ src/Sender/clientSide.py
@@ -3,23 +3,25 @@ from datetime import datetime, time
 import time
 import threading
 import filecmp
-
+import os
 #initializing host, port, filename, total time and number of times to send the file
 serverAddress = "localhost"
-serverPort = 10015
+serverPort = 10032
 #sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 #server_address = (serverAddress, serverPort)
 fileName = "send.txt"
 totalTime = 0
 numTimesSend = 100
 print('I am connecting to server side: ', serverAddress,'\n')
-
+adk = "-1".encode('utf8')
 #using a for loop to send the file 100 times 
 
 for x in range(numTimesSend):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_address = (serverAddress, serverPort)
     fileName = "send.txt"
+    statinfo = os.stat(fileName)
+    
     #recording the start time
     startTime = datetime.now()
     start_time = time.time()
@@ -35,13 +37,40 @@ for x in range(numTimesSend):
     #reading the first 1024 bits
     data = file_to_send.read(1024)
     #sock.sendto(data, server_address)
-    while data:
-        print(x)
+    test = True
+    packetsToSend = statinfo.st_size / 1024
+    #print(file_to_send.tell())
+    print(packetsToSend)
+    for j in range (int(packetsToSend + 1)):
         sent = sock.sendto(data, server_address)
+        j+=1
         print("sending")
         #reading the next 1024 bits
+        sock.settimeout(5)
+        #adk, server = sock.recvfrom(1)
+        #print(adk)
+        #try:
+            #sock.settimeout(5)
+            #adk, server = sock.recvfrom(2)
+            #print(adk)
+            #if adk == 1:
         data = file_to_send.read(1024)
+        if data == b'':
+            test = False
+            print("empty test")
+            break
+                #print("got adk")
+            #else:
+                #print("did not get adk from server")
+        #except:
+            #print("time out reached")
+            #test = False
+            #break            
             
+    #try:
+    sock.settimeout(5)
+    sent = sock.sendto(adk, server_address)
+    print(adk)
     
     print('I am finishing sending file', fileName,' for the ',x,'th  time')
     file_to_send.close()
@@ -50,11 +79,10 @@ for x in range(numTimesSend):
     timeTaken = int((endTime - startTime).total_seconds() * 1000)
     totalTime += timeTaken
     print('The time used in millisecond to receive ', fileName ,' for ', x,'th time is: ',timeTaken,"\n")
+    #except:
     sock.close()
 #sock.close()
 print('The average time to receive file ',fileName,' in millisecond is: ',totalTime/numTimesSend)
 print('Total time to receive file ',fileName,' for ',numTimesSend,' times in millisecond is: ',totalTime)
 print("\nElapsed: " + str(time.time() - start_time))
 print('I am done')
-
-
