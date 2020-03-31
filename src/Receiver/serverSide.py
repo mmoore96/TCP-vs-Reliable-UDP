01ac76486ad117927ffbd3675e6535bbbf3ec26f
George Moore <38229603+mmoore96@users.noreply.github.com>
2020-03-31 15:24:51 -0400

    Update serverSide.py

--- src/Receiver/serverSide.py
+++ src/Receiver/serverSide.py
@@ -7,7 +7,7 @@ from socket import AF_INET, SOCK_DGRAM
 import hashlib
 #initializing host, port
 serverAddress = "localhost"
-serverPort = 10031
+serverPort = 10032
 #starting the server
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.settimeout(15);
@@ -15,16 +15,16 @@ server_address = (serverAddress, serverPort)
 s.bind(server_address)
 totalTime = 0
 print('I am ready for any client side request \n')
-totalFilesCount = 100
+totalFilesCount = 5
 i=0;
 fileName = 'receive.txt';
 adk = 'ok'.encode('utf8')
 timeToStart = 0
+bufferSize = 8192
 while True:
     #s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     #recording the start time
     #s.bind(server_address)
-    startTime = datetime.now()
     i = i+1
     #print(i)
     file = 'receive'+str(i)+'.txt';
@@ -38,7 +38,7 @@ while True:
             sTime = datetime.now()
             s_time = time.time()
             timeToStart = 1
-    data, server = s.recvfrom(1024)
+    data, server = s.recvfrom(bufferSize)
     print('I am starting receiving file ', fileName, 'for the ',i,'th time')
     #data = data
     #print(data)
@@ -52,7 +52,7 @@ while True:
         except:
             print("did not send adk")
             #check = False  
-        data, server = s.recvfrom(1024)
+        data, server = s.recvfrom(bufferSize)
         #print(data)
         if data.decode('utf8') == '-1':
             sent = s.sendto(adk, server)
@@ -61,11 +61,12 @@ while True:
         
         #f.close()
     #f.close()
-    print('I am finishing receiving file ', fileName, 'for the ',i,'th time ')
-    #recording the end time
     endTime = datetime.now()
     timeTaken = int((endTime - startTime).total_seconds() * 1000)
     totalTime += timeTaken
+    print('I am finishing receiving file ', fileName, 'for the ',i,'th time ')
+    #recording the end time
+    
     print('The time used in millisecond to receive ', fileName ,' for ', i,'th time is: ',timeTaken,'\n')
     if i == totalFilesCount:   
         break;
@@ -80,7 +81,7 @@ print('Total time to receive file ',fileName,' for ',totalFilesCount,' times in 
 print("\nElapsed: " + elapsed)
 f=0
 #checking whether the correct data is recieved or not
-hashFun, server = s.recvfrom(1024)
+hashFun, server = s.recvfrom(bufferSize)
 
 s.close()
 for x in range(totalFilesCount):
