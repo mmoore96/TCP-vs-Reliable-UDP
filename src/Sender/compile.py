d64ca42a830304a6600074ebe3a2484825e0a02f
George Moore <38229603+mmoore96@users.noreply.github.com>
2020-04-13 20:39:02 -0400

    Delete compile.py

--- src/Sender/compile.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.system("cd UDPSender; csc -define:DEBUG -out:UDPSender-Debug.exe UDPSender.cs")
-os.system("cd UDPSender; csc -out:UDPSender-Release.exe UDPSender.cs")
-os.system("cd UDPReceiver; csc -define:DEBUG -out:UDPReceiver-Debug.exe UDPReceiver.cs")
-os.system("cd UDPReceiver; csc -out:UDPReceiver-Release.exe UDPReceiver.cs")
