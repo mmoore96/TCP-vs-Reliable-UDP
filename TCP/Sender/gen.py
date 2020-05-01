941bbc5623aac1e8061c8ebd45fd7c05cac3eb67
George Moore <38229603+mmoore96@users.noreply.github.com>
2020-04-30 21:24:32 -0400

    Update gen.py

--- TCP/Sender/gen.py
+++ TCP/Sender/gen.py
@@ -4,4 +4,3 @@ for file in range(3):
     f = open(names[file] + ".txt", 'w')
     for line in range(lines[file]):
         f.write("Here are some words followed by 981 dashes" + "-"*981 + '\n')
-        
