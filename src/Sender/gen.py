a9eddd511c63d5b96e929932df97bdfd6458254c
George Moore <38229603+mmoore96@users.noreply.github.com>
2020-04-30 21:19:26 -0400

    Delete gen.py

--- src/Sender/gen.py
+++
@@ -1,7 +0,0 @@
-names = ["1kb", "1mb", "100mb"]
-lines = [1, 1024, 1024*100]
-for file in range(3):
-    f = open(names[file] + ".txt", 'w')
-    for line in range(lines[file]):
-        f.write("Here are some words followed by 981 dashes" + "-"*981 + '\n')
-        
