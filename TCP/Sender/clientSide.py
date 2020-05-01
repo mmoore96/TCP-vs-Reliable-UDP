6824dfca1a8216c70d1d69991186c73b13ec6e2f
George Moore <38229603+mmoore96@users.noreply.github.com>
2020-04-30 21:23:19 -0400

    Update clientSide.py

--- TCP/Sender/clientSide.py
+++ TCP/Sender/clientSide.py
@@ -3,38 +3,33 @@ from datetime import datetime, time
 import time
 import hashlib
 #initializing host, port, filename, total time and number of times to send the file
-host = '24.214.242.190'
-port = 10002
+host = '000.000.0.00' #PUT the Server IP HERE!
+port = 10002 #Change port number to match your router settings
 fileName = "send.txt"
 totalTime = 0
-numTimesSend = 10
+
+#change depending on how many files you will be sending
+numTimesSend = 20
 bufferSize = 8192
 print('I am connecting to server side: ', host,'\n')
 #using a for loop to send the file 100 times 
 for x in range(numTimesSend):
-    #recording the start time
-    #print("Request started at: " + str(datetime.datetime.utcnow()))
-    #connecting to the server
+    
     s = socket.socket()
-    ##some -1 ?? check is created or not
     s.connect((host, port))
-        
-    #s.send('name.txt'.ljust(100).encode('utf-8'))
     print('I am sending file', fileName,' for the ',x,'th  time')
     #opening file to read
     file_to_send = open(fileName, 'rb')    
-    #reading the first 1024 bits
     startTime = datetime.now()
     start_time = time.time()
     data = file_to_send.read(bufferSize)
     
     while data:
         s.send(data)
-        #reading the next 1024 bits
+        
         data = file_to_send.read(bufferSize)
     print('I am finishing sending file', fileName,' for the ',x,'th  time')
     file_to_send.close()
-    #recording the end time
     endTime = datetime.now()
     timeTaken = int((endTime - startTime).total_seconds() * 1000)
     totalTime += timeTaken
@@ -55,4 +50,4 @@ with open(fileName, 'rb') as afile:
         hasher.update(buf)
         buf = afile.read(BLOCKSIZE)      
 s.send(hasher.hexdigest().encode('utf8'))
-s.close()        
+s.close()
